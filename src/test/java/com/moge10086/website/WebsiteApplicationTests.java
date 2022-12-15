d99277a304aebe95a99cc60765260041a5df79b1
moge10086 <2287247953@qq.com>
2022-12-15 14:43:55 +0800

    项目初始化

---
+++ src/test/java/com/moge10086/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moge10086.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
